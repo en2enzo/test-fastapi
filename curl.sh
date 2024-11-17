a9f390f2678ce645c9029b0f6010cfbd7eb6bda2
en2enzo <pikaliza@aol.com>
2024-11-17 08:31:44 -0500

    test

---
+++ curl.sh
@@ -0,0 +1,4 @@
+curl -X 'GET' \
+  'https://testfastapi-j24lctj0.b4a.run/' \
+  -H 'accept: application/json' \
+  -H 'Authorization: expected_token'
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import APIKeyHeader
+import os
 
 #app = FastAPI()
 
+token = os.environ['API_TOKEN']
 
 api_key_header = APIKeyHeader(name="Authorization", auto_error=True)
 
 def verify_token(auth_header: str = Depends(api_key_header)):
-    if auth_header != "expected_token":
+    #if auth_header != "expected_token":
+    if auth_header != token:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Invalid authentication token",
