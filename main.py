13b73d7f40299c33ba82f943d4da3f479fb52200
en2enzo <pikaliza@aol.com>
2024-11-17 08:03:24 -0500

    test3

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import APIKeyHeader
 
-app = FastAPI()
+#app = FastAPI()
+
+
+api_key_header = APIKeyHeader(name="Authorization", auto_error=True)
+
+def verify_token(auth_header: str = Depends(api_key_header)):
+    if auth_header != "expected_token":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Invalid authentication token",
+        )
+
+
+app = FastAPI(
+    title="test-fastapi",
+    version="1.0.0",
+    openapi_tags=[
+        {
+            "name": "todos",
+            "description": "タスク管理に関する操作。",
+        },
+        {
+            "name": "users",
+            "description": "ユーザー管理に関する操作。",
+        },
+    ],
+    dependencies=[Depends(verify_token)],
+)
+
 
 @app.get("/")
 async def get_hello():
